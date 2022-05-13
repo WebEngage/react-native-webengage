783eac92162e4ff6f463c18ad661eaf0c765a123
webklipper <sarthak.sharma@webklipper.com>
2022-05-13 19:46:59 +0530

    v1.2.12 Changing name of hasListener as WE variable.

--- lib/ios/WEGWebEngageBridge.m
+++ lib/ios/WEGWebEngageBridge.m
@@ -13,7 +13,7 @@
 
 NSString * const DATE_FORMAT = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 int const DATE_FORMAT_LENGTH = 24;
-bool hasListeners = NO;
+bool weHasListeners = NO;
 
 @implementation WEGWebEngageBridge
 
@@ -255,7 +255,7 @@ RCT_EXPORT_METHOD(logout){
 -(void)WEGHandleDeeplink:(NSString *)deeplink userData:(NSDictionary *)data{
     RCTLogInfo(@"webengageBridge: push notification clicked with deeplink: %@", deeplink);
     NSDictionary *pushData = @{@"deeplink":deeplink, @"userData":data};
-     if (hasListeners) {
+     if (weHasListeners) {
         [self sendEventWithName:@"pushNotificationClicked" body:pushData];
     } else {
         if (self.pendingEventsDict == nil) {
@@ -270,7 +270,7 @@ RCT_EXPORT_METHOD(logout){
 - (void)sendUniversalLinkLocation:(NSString *)location{
     RCTLogInfo(@"webengageBridge: universal link clicked with location: %@", location);
     NSDictionary *data = @{@"location":location};
-    if (hasListeners) {
+    if (weHasListeners) {
         [self sendEventWithName:@"universalLinkClicked" body:data];
     } else {
         if (self.pendingEventsDict == nil) {
@@ -285,7 +285,7 @@ RCT_EXPORT_METHOD(logout){
 
 // Will be called when this module's first listener is added.
 - (void) startObserving {
-    hasListeners = YES;
+    weHasListeners = YES;
     if (self.pendingEventsDict != nil) {
         for (id key in self.pendingEventsDict) {
             [self sendEventWithName:key body:self.pendingEventsDict[key]];
@@ -295,6 +295,6 @@ RCT_EXPORT_METHOD(logout){
 }
 
 - (void)stopObserving {
-    hasListeners = NO;
+    weHasListeners = NO;
 }
 @end

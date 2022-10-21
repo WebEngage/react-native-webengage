bb5a255d9833ac3c381459ddae8fcaf052364bb8
Milind Keni <kenimilind864@gmail.com>
2022-10-21 20:47:20 +0530

    async storage bug fix

--- ExampleApp/App.js
+++ ExampleApp/App.js
@@ -6,18 +6,21 @@
  * @flow strict-local
  */
 
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import type {Node} from 'react';
 import {
   Linking,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet, Switch,
-  Text, TextInput, TouchableHighlight,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableHighlight,
   useColorScheme,
   View,
-} from "react-native";
+} from 'react-native';
 
 import {
   Colors,
@@ -26,18 +29,18 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import WebEngage from "react-native-webengage";
+import WebEngage from 'react-native-webengage';
 import Toast from 'react-native-simple-toast';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = false;
   return (
-    <View style={styles.sectionContainer}>
+    <View style={{...styles.sectionContainer, backgroundColor: 'white'}}>
       <Text
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
+            color: isDarkMode ? Colors.white : Colors.white,
           },
         ]}>
         {title}
@@ -46,7 +49,7 @@ const Section = ({children, title}): Node => {
         style={[
           styles.sectionDescription,
           {
-            color: isDarkMode ? Colors.light : Colors.dark,
+            color: isDarkMode ? Colors.light : Colors.white,
           },
         ]}>
         {children}
@@ -67,64 +70,61 @@ export default class App extends Component<Props> {
       loginButtonText: 'LOGIN',
       event: '',
       isPushEnabled: true,
-      isInappEnabled: true
-    }
+      isInappEnabled: true,
+    };
     this.login = this.login.bind(this);
     this.track = this.track.bind(this);
     this.buy = this.buy.bind(this);
     this.togglePushSwitch = this.togglePushSwitch.bind(this);
     this.toggleInappSwitch = this.toggleInappSwitch.bind(this);
-    console.log("MyLogs: In constructor")
+    console.log('MyLogs: In constructor');
   }
 
   componentDidMount() {
-    console.log("MyLogs: In componentDidMount")
+    console.log('MyLogs: In componentDidMount');
     Toast.show('This is a toast.');
     Linking.addEventListener('url', this.handleOpenURL);
 
     try {
-      AsyncStorage.getItem('userid')
-        .then((user_id) => {
-          console.log("user id: " + user_id);
-          if (user_id && user_id !== null && user_id !== '') {
-            console.log("logged in user id: " + user_id);
-            this.setState({
-              userId: user_id,
-              userIdInput: user_id,
-              loginButtonText: "LOGOUT"
-            })
-          } else {
-            this.setState({
-              loginButtonText: "LOGIN"
-            })
-          }
-        });
-
-      AsyncStorage.getItem('push_optin')
-        .then((value) => {
-          var isEnabled = JSON.parse(value);
-          console.log('Initial push_optin: ' + isEnabled);
-          if (isEnabled === null) {
-            isEnabled = true;
-            AsyncStorage.setItem('push_optin', JSON.stringify(true));
-          }
+      AsyncStorage.getItem('userid').then(user_id => {
+        console.log('user id: ' + user_id);
+        if (user_id && user_id !== null && user_id !== '') {
+          console.log('logged in user id: ' + user_id);
           this.setState({
-            isPushEnabled: isEnabled
+            userId: user_id,
+            userIdInput: user_id,
+            loginButtonText: 'LOGOUT',
           });
-        });
-
-      AsyncStorage.getItem('inapp_optin')
-        .then((value) => {
-          var isEnabled = JSON.parse(value);
-          console.log('Initial inapp_optin: ' + isEnabled);
-          if (isEnabled === null) {
-            isEnabled = true;
-            AsyncStorage.setItem('inapp_optin', JSON.stringify(true));
-          }
+        } else {
           this.setState({
-            isInappEnabled: isEnabled
+            loginButtonText: 'LOGIN',
           });
+        }
+      });
+
+      AsyncStorage.getItem('push_optin').then(value => {
+        var isEnabled = JSON.parse(value);
+        console.log('Initial push_optin: ' + isEnabled);
+        if (isEnabled === null) {
+          isEnabled = true;
+          AsyncStorage.setItem('push_optin', JSON.stringify(true));
+        }
+        this.setState({
+          isPushEnabled: isEnabled,
         });
+      });
+
+      AsyncStorage.getItem('inapp_optin').then(value => {
+        var isEnabled = JSON.parse(value);
+        console.log('Initial inapp_optin: ' + isEnabled);
+        if (isEnabled === null) {
+          isEnabled = true;
+          AsyncStorage.setItem('inapp_optin', JSON.stringify(true));
+        }
+        this.setState({
+          isInappEnabled: isEnabled,
+        });
+      });
     } catch (error) {
       console.log(error);
     }
@@ -133,38 +133,40 @@ export default class App extends Component<Props> {
     //webengage.screen("Home");
 
     // In-app notification callbacks
-    webengage.notification.onPrepare(function(notificationData) {
+    webengage.notification.onPrepare(function (notificationData) {
       Toast.show('InApp :  onPrepare');
     });
 
-    webengage.notification.onShown(function(notificationData) {
+    webengage.notification.onShown(function (notificationData) {
       var message;
-      if (notificationData["title"] && notificationData["title"] !== null) {
-        message = "title: " + notificationData["title"];
-      } else if (notificationData["description"] && notificationData["description"] !== null) {
-        message = "description: " + notificationData["description"];
+      if (notificationData['title'] && notificationData['title'] !== null) {
+        message = 'title: ' + notificationData['title'];
+      } else if (
+        notificationData['description'] &&
+        notificationData['description'] !== null
+      ) {
+        message = 'description: ' + notificationData['description'];
       }
       Toast.show('InApp :  onShown -->' + message);
     });
 
-    webengage.notification.onClick(function(notificationData, clickId) {
+    webengage.notification.onClick(function (notificationData, clickId) {
       //console.log("App: in-app notification clicked: click-id: " + clickId + ", deep-link: " + notificationData["deeplink"]);
       Toast.show('InApp :  onClick -->' + clickId);
-      Toast.show(', deep-link: ' + notificationData["deeplink"]);
+      Toast.show(', deep-link: ' + notificationData['deeplink']);
     });
 
-    webengage.notification.onDismiss(function(notificationData) {
+    webengage.notification.onDismiss(function (notificationData) {
       Toast.show('InApp :  onDismiss -->');
     });
 
-    webengage.push.onClick(function(notificationData) {
+    webengage.push.onClick(function (notificationData) {
       // console.log("MyLogs App: push-notiifcation clicked with deeplink: " + notificationData["deeplink"]);
       // console.log("MyLogs App: push-notiifcation clicked with payload: " + JSON.stringify(notificationData["userData"]));
-      Toast.show('Push :  onClick -->'+ notificationData["deeplink"]);
-
+      Toast.show('Push :  onClick -->' + notificationData['deeplink']);
     });
-    webengage.universalLink.onClick(function(location){
-      console.log("App: universal link clicked with location: " + location);
+    webengage.universalLink.onClick(function (location) {
+      console.log('App: universal link clicked with location: ' + location);
       notifyMessage(location);
     });
   }
@@ -174,47 +176,57 @@ export default class App extends Component<Props> {
   }
 
   handleOpenURL(event) {
-    console.log("App: launch URL: " + event.url);
+    console.log('App: launch URL: ' + event.url);
   }
 
   render() {
-    console.log("App: Render called");
+    console.log('App: Render called');
     return (
       <View>
         <ScrollView>
           <View style={styles.container}>
             <TextInput
-              style={{width: 250,height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10}}
-              onChangeText={(text) => this.setState({userIdInput: text})}
+              style={{
+                width: 250,
+                height: 40,
+                borderColor: 'gray',
+                borderWidth: 1,
+                marginBottom: 10,
+              }}
+              onChangeText={text => this.setState({userIdInput: text})}
               value={this.state.userIdInput}
             />
 
-            <TouchableHighlight style={styles.button}
-                                onPress={this.login}>
+            <TouchableHighlight style={styles.button} onPress={this.login}>
               <Text>{this.state.loginButtonText}</Text>
             </TouchableHighlight>
 
             <TextInput
-              style={{width: 250,height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 50, marginBottom: 10}}
-              onChangeText={(text) => this.setState({event: text})}
+              style={{
+                width: 250,
+                height: 40,
+                borderColor: 'gray',
+                borderWidth: 1,
+                marginTop: 50,
+                marginBottom: 10,
+              }}
+              onChangeText={text => this.setState({event: text})}
               value={this.state.event}
             />
 
-            <TouchableHighlight style={styles.button}
-                                onPress={this.track}>
+            <TouchableHighlight style={styles.button} onPress={this.track}>
               <Text>TRACK</Text>
             </TouchableHighlight>
 
-            <TouchableHighlight style={styles.button}
-                                onPress={this.buy}>
+            <TouchableHighlight style={styles.button} onPress={this.buy}>
               <Text>BUY NOW</Text>
             </TouchableHighlight>
 
             <View style={styles.channel}>
               <Text style={styles.label}>Push</Text>
               <Switch
-                trackColor={{ true: "skyblue", false: "#c4c4c4" }}
-                thumbColor={this.state.isPushEnabled ? "lightblue" : "#e1e1e1"}
+                trackColor={{true: 'skyblue', false: '#c4c4c4'}}
+                thumbColor={this.state.isPushEnabled ? 'lightblue' : '#e1e1e1'}
                 ios_backgroundColor="#3e3e3e"
                 onValueChange={this.togglePushSwitch}
                 value={this.state.isPushEnabled}
@@ -224,8 +236,8 @@ export default class App extends Component<Props> {
             <View style={styles.channel}>
               <Text style={styles.label}>In-app</Text>
               <Switch
-                trackColor={{ true: "skyblue", false: "#f1f1f1" }}
-                thumbColor={this.state.isInappEnabled ? "lightblue" : "#e4e4e4"}
+                trackColor={{true: 'skyblue', false: '#f1f1f1'}}
+                thumbColor={this.state.isInappEnabled ? 'lightblue' : '#e4e4e4'}
                 ios_backgroundColor="#3e3e3e"
                 onValueChange={this.toggleInappSwitch}
                 value={this.state.isInappEnabled}
@@ -238,7 +250,7 @@ export default class App extends Component<Props> {
   }
 
   login() {
-    webengage.user.setDevicePushOptIn(true);
+    // webengage.user.setDevicePushOptIn(true);
     if (this.state.userId === undefined || this.state.userId === null) {
       // Login
       var newUserId = this.state.userIdInput;
@@ -249,12 +261,12 @@ export default class App extends Component<Props> {
 
         this.setState({
           userId: newUserId,
-          loginButtonText: "LOGOUT"
-        })
+          loginButtonText: 'LOGOUT',
+        });
 
-        console.log("App: Login called");
+        console.log('App: Login called');
       } else {
-        console.log("App: Invalid user id");
+        console.log('App: Invalid user id');
       }
     } else {
       // Logout
@@ -265,51 +277,53 @@ export default class App extends Component<Props> {
       this.setState({
         userId: null,
         userIdInput: '',
-        loginButtonText: "LOGIN"
-      })
+        loginButtonText: 'LOGIN',
+      });
 
-      console.log("App: Logout called");
+      console.log('App: Logout called');
     }
   }
 
   track() {
-    if (this.state.event && this.state.event !== null && this.state.event != '') {
+    if (
+      this.state.event &&
+      this.state.event !== null &&
+      this.state.event != ''
+    ) {
       webengage.track(this.state.event);
     }
   }
 
   buy() {
-    var event = "Product Purchased";
+    var event = 'Product Purchased';
     var attributes = {
-      "Amount": 2300,
+      Amount: 2300,
       //"Delivery Date": new Date("2017-01-09T16:30:00.000Z"),
       //"Delivery Date": new Date("Thu Jun 20 2019 09:30:00 GMT+0530 (India Standard Time)"),
-      "Delivery Date": new Date(),
-      "Products" : [
+      'Delivery Date': new Date(),
+      Products: [
         {
-          "SKU Code": "UHUH799",
-          "Product Name": "Armani Jeans",
-          "Price": 300.49,
-          "Details": {
-            "Size": "L"
-          }
+          'SKU Code': 'UHUH799',
+          'Product Name': 'Armani Jeans',
+          Price: 300.49,
+          Details: {
+            Size: 'L',
+          },
         },
         {
-          "SKU Code": "FBHG746",
-          "Product Name": "Hugo Boss Jacket",
-          "Price": 507.99,
-          "Details": {
-            "Size": "L",
-          }
+          'SKU Code': 'FBHG746',
+          'Product Name': 'Hugo Boss Jacket',
+          Price: 507.99,
+          Details: {
+            Size: 'L',
+          },
         },
       ],
-      "Delivery Address": {
-        "City": "San Francisco",
-        "ZIP": "94121"
+      'Delivery Address': {
+        City: 'San Francisco',
+        ZIP: '94121',
       },
-      "Coupons Applied": [
-        "BOGO17"
-      ]
+      'Coupons Applied': ['BOGO17'],
     };
     webengage.track(event, attributes);
   }
@@ -318,18 +332,18 @@ export default class App extends Component<Props> {
     var isEnabled = !this.state.isPushEnabled;
     console.log('Push switch toggled: ' + isEnabled);
     this.setState({
-      isPushEnabled: isEnabled
+      isPushEnabled: isEnabled,
     });
-    webengage.user.setOptIn("push", isEnabled);
+    webengage.user.setOptIn('push', isEnabled);
   }
 
   toggleInappSwitch() {
     var isEnabled = !this.state.isInappEnabled;
     console.log('In-app switch toggled: ' + isEnabled);
     this.setState({
-      isInappEnabled: isEnabled
+      isInappEnabled: isEnabled,
     });
-    webengage.user.setOptIn("in_app", isEnabled);
+    webengage.user.setOptIn('in_app', isEnabled);
     //webengage.user.setOptIn("email", isEnabled);
     //webengage.user.setOptIn("sms", isEnabled);
     //webengage.user.setOptIn("whatsapp", isEnabled);
@@ -342,7 +356,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-    marginTop: 80
+    marginTop: 80,
   },
   welcome: {
     fontSize: 20,
@@ -359,16 +373,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     padding: 10,
-    marginBottom: 25
+    marginBottom: 25,
   },
   channel: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     width: 144,
-    marginTop: 16
+    marginTop: 16,
   },
   label: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });

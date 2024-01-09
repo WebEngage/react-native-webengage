0ca868bbd4a6490c4c47d8c0e3d89bbb750aeaa3
Akshay <akshay.chilad@webengage.com>
2024-01-09 15:30:26 +0530

    Removed Old Sample App

--- ExampleApp/ios/ExampleApp/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
-#import <WEGWebEngageBridge.h>
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-@property (nonatomic, strong) WEGWebEngageBridge *bridge;
-
-@end

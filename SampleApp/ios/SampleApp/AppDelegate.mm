5f7715231caecd977d493bdfe79fd145607ba66e
Akshay <akshay.chilad@webengage.com>
2024-01-18 13:38:43 +0530

    iOS push callbacks integrated

--- SampleApp/ios/SampleApp/AppDelegate.mm
+++ SampleApp/ios/SampleApp/AppDelegate.mm
@@ -6,10 +6,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"SampleApp";
+  self.weBridge = [WEGWebEngageBridge new];
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self.weBridge launchOptions:launchOptions];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  [[WebEngage sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+  [WebEngage sharedInstance].pushNotificationDelegate = self.weBridge;
+      [[WebEngage sharedInstance] application:application
+              didFinishLaunchingWithOptions:launchOptions notificationDelegate:self.weBridge];
     
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];

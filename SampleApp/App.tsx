db47bb95a50589a5c3bca626ec4ce0f177d36aa0
Akshay <akshay.chilad@webengage.com>
2024-03-11 19:33:28 +0530

    HeadlessJS implementation for Android - OnPushReceived

--- SampleApp/App.tsx
+++ SampleApp/App.tsx
@@ -4,12 +4,35 @@ import {
   enableDevMode,
   initWENotificationInbox,
 } from 'react-native-webengage-inbox';
-import {initWebEngage} from './src/WebEngageHandler/WebEngageManager';
+import {
+  disableWebEngagePush,
+  enableWebEngagePush,
+  initWebEngage,
+} from './src/WebEngageHandler/WebEngageManager';
+import {PermissionsAndroid} from 'react-native';
 
 function App(): React.JSX.Element {
   enableDevMode();
   initWebEngage();
   initWENotificationInbox();
+  // Rquesting permission for Android
+  const requestAndroidPermissions = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        enableWebEngagePush();
+        console.log('Android permissions granted');
+      } else {
+        disableWebEngagePush();
+        console.log('Android permissions denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  requestAndroidPermissions();
 
   return <AppNavigator />;
 }
--- SampleApp/index.js
+++ SampleApp/index.js
@@ -2,5 +2,15 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 // TODO change source to exact App
 import {name as appName} from './app.json';
+import webEngageManager from './src/WebEngageHandler/WebEngageManager';
+import CONSTANTS from './src/Utils/Constants';
 
+// This method has to be called in index.js even before app is registered in AppRegistry
+//  This handles Foreground/Killed/Background state
+webEngageManager.push.onPushReceived(async function (notificationData) {
+  console.log(
+    CONSTANTS.WEBENGAGE_PUSH + 'Push notification received in index.js: ',
+    JSON.stringify(notificationData),
+  );
+});
 AppRegistry.registerComponent(appName, () => App);

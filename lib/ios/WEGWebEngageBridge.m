2967e61a5ba9fe92518853b03d12437bf44bbea0
Milind Keni <milind.keni>
2025-05-24 18:05:36 +0530

    version change to 1.6.0

--- lib/ios/WEGWebEngageBridge.m
+++ lib/ios/WEGWebEngageBridge.m
@@ -14,7 +14,7 @@
 NSString * const DATE_FORMAT = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 int const DATE_FORMAT_LENGTH = 24;
 bool weHasListeners = NO;
-NSString *WEGPluginVersion = @"1.5.4";
+NSString *WEGPluginVersion = @"1.6.0";
 
 @implementation WEGWebEngageBridge
 

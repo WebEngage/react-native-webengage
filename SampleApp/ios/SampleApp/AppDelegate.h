6e3dab1ac06f255646120ed754bf8707b065fb5e
Milind Keni <milind.keni>
2026-01-16 14:55:58 +0530

    lib-at-root v2

--- SampleApp/ios/SampleApp/AppDelegate.h
+++
@@ -1,8 +0,0 @@
-#import <React-RCTAppDelegate/RCTAppDelegate.h>
-#import <WebEngage/WebEngage.h>
-#import <UIKit/UIKit.h>
-#import <WEGWebEngageBridge.h>
-
-@interface AppDelegate : RCTAppDelegate
-@property (nonatomic, strong) WEGWebEngageBridge *weBridge; // Docs: Add this line to Docs
-@end
--- SampleApp/react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dependencies: {
-    'react-native-we-personalization': {
-      platforms: {
-        android: null,
-      },
-    },
-  },
-};

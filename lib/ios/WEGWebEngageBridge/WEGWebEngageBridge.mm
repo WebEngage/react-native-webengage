c6ae8d5824553ade007d6ee65f6d561466d82d77
Akshay <akshay.chilad@webengage.com>
2026-05-21 13:11:55 +0530

    bumped react-native-webengage package version to 2.0.0

--- lib/ios/WEGWebEngageBridge/WEGWebEngageBridge.mm
+++ lib/ios/WEGWebEngageBridge/WEGWebEngageBridge.mm
@@ -13,7 +13,7 @@
 NSString * const DATE_FORMAT = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 int const DATE_FORMAT_LENGTH = 24;
 static BOOL weHasListeners = NO;
-NSString *WEGPluginVersion = @"1.5.1";
+NSString *WEGPluginVersion = @"2.0.0";
 
 @implementation WEGWebEngageBridge
 

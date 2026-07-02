86a4d4973bab8c541b2f3a2b1452e3c703abae0c
Shubham Naidu <shubham.naidu@webengage.com>
2026-07-03 03:53:06 +0530

    AI Review Changes

--- lib/ios/add_webengage_spm.rb
+++ lib/ios/add_webengage_spm.rb
@@ -4,15 +4,19 @@
 # Usage in Podfile (single line - no need to call in post_install):
 #   require_relative '../node_modules/react-native-webengage/ios/add_webengage_spm'
 
-require 'xcodeproj'
+begin
+  require 'xcodeproj'
+rescue LoadError
+  raise "[react-native-webengage] The 'xcodeproj' gem is required. Run: sudo gem install xcodeproj"
+end
 
-WEBENGAGE_REPO_URL = 'https://github.com/WebEngage/webengage-ios-sdk'
-WEBENGAGE_BRANCH = 'main'
-WEBENGAGE_PRODUCT_NAME = 'WebEngageCore'
+WEBENGAGE_REPO_URL = defined?(WEBENGAGE_REPO_URL) ? WEBENGAGE_REPO_URL : 'https://github.com/WebEngage/webengage-ios-sdk'
+WEBENGAGE_MIN_VERSION = defined?(WEBENGAGE_MIN_VERSION) ? WEBENGAGE_MIN_VERSION : '2.0.0'
+WEBENGAGE_PRODUCT_NAME = defined?(WEBENGAGE_PRODUCT_NAME) ? WEBENGAGE_PRODUCT_NAME : 'WebEngageCore'
 
 def add_webengage_spm(installer, options = {})
   repo_url = options[:repo_url] || WEBENGAGE_REPO_URL
-  branch = options[:branch] || WEBENGAGE_BRANCH
+  min_version = options[:min_version] || WEBENGAGE_MIN_VERSION
   product_name = options[:product_name] || WEBENGAGE_PRODUCT_NAME
 
   project = nil
@@ -28,30 +32,38 @@ def add_webengage_spm(installer, options = {})
     return
   end
 
+  project_modified = false
+
   pkg_ref = project.root_object.package_references.find { |ref| ref.repositoryURL == repo_url }
   if pkg_ref
     puts "[react-native-webengage] WebEngage SPM package reference already present."
   else
     pkg_ref = project.new(Xcodeproj::Project::Object::XCRemoteSwiftPackageReference)
     pkg_ref.repositoryURL = repo_url
-    pkg_ref.requirement = { 'kind' => 'branch', 'branch' => branch }
+    pkg_ref.requirement = { 'kind' => 'upToNextMajorVersion', 'minimumVersion' => min_version }
     project.root_object.package_references << pkg_ref
-    puts "[react-native-webengage] Added WebEngage SPM package reference (branch: #{branch})."
+    puts "[react-native-webengage] Added WebEngage SPM package reference (>= #{min_version})."
+    project_modified = true
   end
 
   app_target = project.targets.find { |t| t.product_type == 'com.apple.product-type.application' }
-  if app_target
-    existing = app_target.package_product_dependencies.any? { |d| d.product_name == product_name }
-    unless existing
-      dep = project.new(Xcodeproj::Project::Object::XCSwiftPackageProductDependency)
-      dep.package = pkg_ref
-      dep.product_name = product_name
-      app_target.package_product_dependencies << dep
-      puts "[react-native-webengage] Added SPM product: #{product_name}"
-    end
+  unless app_target
+    puts "[react-native-webengage] Could not find application target, skipping SPM product setup."
+    return
   end
 
-  project.save
+  existing = app_target.package_product_dependencies.any? { |d| d.product_name == product_name }
+  unless existing
+    dep = project.new(Xcodeproj::Project::Object::XCSwiftPackageProductDependency)
+    dep.package = pkg_ref
+    dep.product_name = product_name
+    app_target.package_product_dependencies << dep
+    puts "[react-native-webengage] Added SPM product: #{product_name}"
+    project_modified = true
+  end
+
+  # only save if something actually changed
+  project.save if project_modified
 end
 
 # Auto-register into CocoaPods post_install hook
--- lib/react-native-webengage.podspec
+++ lib/react-native-webengage.podspec
@@ -18,32 +18,20 @@ Pod::Spec.new do |s|
   s.preserve_paths = 'LICENSE.md', 'README.md', 'package.json', 'src/**/*', 'types/**/*'
   s.source_files   = 'ios/WEGWebEngageBridge/*.{h,m,mm}'
   s.public_header_files = 'ios/WEGWebEngageBridge/WEGWebEngageBridge.h'
-  s.pod_target_xcconfig  = { 'DEFINES_MODULE' => 'YES' }
-  
+
+  # WebEngage native SDK is provided via Swift Package Manager (SPM).
+  # URL: https://github.com/WebEngage/webengage-ios-sdk
+  s.pod_target_xcconfig = {
+    'DEFINES_MODULE' => 'YES',
+    'FRAMEWORK_SEARCH_PATHS' => '"$(PODS_CONFIGURATION_BUILD_DIR)"',
+    'HEADER_SEARCH_PATHS' => '"$(PODS_CONFIGURATION_BUILD_DIR)/WebEngageCore.framework/Headers"',
+    'OTHER_LDFLAGS' => '-framework WebEngageCore'
+  }
+
   if respond_to?(:install_modules_dependencies, true)
     install_modules_dependencies(s)
   else
     s.dependency 'React-Core'
   end
-
-  s.dependency 'React-Core'
-
-  # WebEngage native SDK is now provided via Swift Package Manager (SPM).
-  # Add the WebEngage SPM package in your Xcode project:
-  #   URL: https://github.com/WebEngage/webengage-ios-sdk
-
-  # Allow the bridge code to find SPM-provided WebEngage framework headers
-  s.pod_target_xcconfig = {
-    'FRAMEWORK_SEARCH_PATHS' => '"$(PODS_CONFIGURATION_BUILD_DIR)"',
-    'HEADER_SEARCH_PATHS' => '"$(PODS_CONFIGURATION_BUILD_DIR)/WebEngage.framework/Headers"',
-    'OTHER_LDFLAGS' => '-framework WebEngage'
-  }
-  
-  #Keeping the below code for now, to be removed when releasing 
-  # if ENV['WEBENGAGE_USE_CORE'] == 'true'
-  #   s.dependency 'WebEngage/Core','>= 6.16.6'
-  # else
-  #   s.dependency 'WebEngage','>= 6.16.6'
-  # end
 
 end
